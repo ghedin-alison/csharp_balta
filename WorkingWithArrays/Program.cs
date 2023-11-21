82e3092973f23a71ba0ab6f3d5cc6c4a3daf6e20
Alison Ghedin <alison.ghedin@outlook.com>
2023-11-20 22:10:14 -0300

    add WorkingWithArray

---
+++ WorkingWithArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Clear();
+
+var minhaArray = new int[5]{7,2,3,4,5};
+
+
+Console.WriteLine(minhaArray[0]);
+
+
+var outraArray = new Teste[2];
+outraArray[0] = new Teste();
+outraArray[1] = new Teste();
+
+Console.WriteLine(outraArray[0].Id);
+
+//Segunda Aula
+Console.Clear();
+
+
+
+struct Teste
+{
+    public int Id { get; set; }
+}
+
+
