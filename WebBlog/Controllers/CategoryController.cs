31f1fe04256b021d6e018cfc6f2f5189f721a64a
Alison Ghedin <alison.ghedin@outlook.com>
2024-03-07 23:36:04 -0300

    api basica em dotnet

--- WebBlog/Controllers/CategoryController.cs
+++ WebBlog/Controllers/CategoryController.cs
@@ -1,16 +1,106 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebBlog.Data;
+using WebBlog.Models;
 
 namespace WebBlog.Controllers;
 
 [ApiController]
 public class CategoryController: ControllerBase
 {
+    // Get All
     [HttpGet("v1/categories")]
     public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
     {
         var categories = await context.Categories.ToListAsync();
         return Ok(categories);
+    }
+
+    // Get One
+    [HttpGet("v1/categories/{id:int}")]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromRoute] int id,
+        [FromServices] BlogDataContext context)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        
+        if(category == null)
+            return NotFound();
+        
+        return Ok(category);
+    }
+
+    //Post
+    [HttpPost("v1/categories")]
+    public async Task<IActionResult> PostAsync(
+        [FromBody] Category model,
+        [FromServices] BlogDataContext context)
+    {
+
+        try
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateException e)
+        {
+            return StatusCode(500, "05XA1 - Não foi possível incluir a categoria.");
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "05XA2 - Falha interna no servidor");
+        }
+        
+        return Created($"v1/categories/{model.Id}", model);
+    }
+    
+    //Put
+    [HttpPut("v1/categories/{id:int}")]
+    public async Task<IActionResult> PutAsync(
+        [FromRoute] int id,
+        [FromBody] Category model,
+        [FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "Não foi possível alterar.");
+        }
+    }
+    
+    // Delete
+    [HttpDelete("v1/categories/{id:int}")]
+    public async Task<IActionResult> DeleteAsync(
+        [FromRoute] int id,
+        [FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, "Não foi possível excluir");
+        }
+
     }
 }
--- WebBlog/Data/BlogDataContext.cs
+++ WebBlog/Data/BlogDataContext.cs
@@ -8,11 +8,7 @@ namespace WebBlog.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; }
-        // public DbSet<Role> Roles { get; set; }
-        // public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
-        // public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlServer("Server=localhost,1433;Database=WebBlog;User ID=sa;Password=1q2w3e4r@#$;encrypt=False");
