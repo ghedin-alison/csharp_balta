653618ffa1e265085e2f703caa8f06b116b99c9c
Alison Ghedin <alison.ghedin@outlook.com>
2024-03-25 23:14:29 -0300

    erro na autenticacao

--- WebBlog/Controllers/AccountController.cs
+++ WebBlog/Controllers/AccountController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
@@ -55,11 +54,36 @@ public class AccountController:ControllerBase
         
     }
     
-    [HttpPost("v1/login")]
-    public IActionResult Login([FromServices] TokenService tokenService)
+    [HttpPost("v1/accounts/login")]
+    public async Task<IActionResult> Login(
+        [FromBody] LoginViewModel model,
+        [FromServices] BlogDataContext context,
+        [FromServices] TokenService tokenService)
     {
-        var token = tokenService.GenerateToken(null);
-        return Ok(token);
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+!!!Erro Aqui!!
+        var user = await context
+            .Users
+            .AsNoTracking()
+            .Include(x => x.Roles)
+            .FirstOrDefault(x => x.Email == model.Email);
+        
+        if (user == null)
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválido."));
+
+        if(!PasswordHasher.Verify(user.Password, model.Password));
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou password inválido."));
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, null));
+            }
+            catch 
+            {
+                return StatusCode(500, new ResultViewModel<string>("0x504 - Falha interna no servidor."));
+                throw;
+            }
     }
     
 
