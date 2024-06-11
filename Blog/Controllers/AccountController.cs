5912ea19dbe54856c882c247a6d53266bd57234b
Alison Ghedin <alison.ghedin@outlook.com>
2024-06-10 21:29:12 -0300

    refatorando a injeção de dependencia

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -6,16 +6,11 @@ namespace Blog.Controllers;
 [ApiController]
 public class AccountController : ControllerBase
 {
-    private readonly TokenService _tokenService;
-    public AccountController(TokenService tokenService)
-    {
-        _tokenService = tokenService;
-    }// injeção de dependencia pra fazer login preciso de um token service
     
     [HttpPost("v1/login")]
-    public IActionResult Login()
+    public IActionResult Login([FromServices] TokenService tokenService)
     {
-        var token = _tokenService.GenerateToken(user: null);
+        var token = tokenService.GenerateToken(user: null);
 
         return Ok(token);
 
